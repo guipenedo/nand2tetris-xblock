836e71d59e8f2c579203ecb7902160057ef78b72
guipenedo <nostrumg@gmail.com>
2021-02-07 17:54:14 +0100

    update docker image version

--- nand2tetris/nand2tetris.py
+++ nand2tetris/nand2tetris.py
@@ -39,7 +39,7 @@ ITEM_TYPE = "nand2tetrisxblock"
 
 epicbox.configure(
     profiles=[
-        epicbox.Profile('nand2tetris', 'tcarreira/nand2tetris-autograder:epicbox-0.0.10')
+        epicbox.Profile('nand2tetris', 'tcarreira/nand2tetris-autograder:2.6.1-epicbox')
     ]
 )
 limits = {'cputime': 1, 'memory': 64}
